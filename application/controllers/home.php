5a1577fba84568a971f5924a6f4c382e255686a8
jaspherIntes <jazzintes06@gmail.com>
2014-11-22 21:35:58 +0800

    login

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,22 +1,34 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+session_start(); //we need to call PHP's session object to access it through CI
 class Home extends CI_Controller {
-
-	public function __construct()
-	{
-		parent::__construct();
-
-		$this->load->model('Register');
-		$this->load->library('form_validation');
-		$this->load->helper('url');
-	}
-
-	function index()
-	{
-		// $this->load->view('includes/header');
-		// $this->load->view('includes/menu');
-		$this->load->view('home');
-		// $this->load->view('includes/footer');
-	}
-
+ 
+ function __construct()
+ {
+   parent::__construct();
+ }
+ 
+ function index()
+ {
+   if($this->session->userdata('logged_in'))
+   {
+     $session_data = $this->session->userdata('logged_in');
+     $data['username'] = $session_data['username'];
+     $this->load->view('home_view', $data);
+   }
+   else
+   {
+     //If no session, redirect to login page
+     redirect('login', 'refresh');
+   }
+ }
+ 
+ function logout()
+ {
+   $this->session->unset_userdata('logged_in');
+   session_destroy();
+   redirect('home', 'refresh');
+ }
+ 
 }
+ 
 ?>
